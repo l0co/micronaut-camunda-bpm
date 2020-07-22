1e560b33bfd68b0b53f4744c4222e8578ce1f2a6
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2020-07-22 10:27:04 +0200

    managed transaction support

---
+++ micronaut-camunda-bpm-feature/transactional-test/README.md
@@ -0,0 +1,8 @@
+# Transactional test
+
+Additional tests using Micronaut-managed transaction manager and datasource with the following additional depedencies:
+
+```
+testImplementation("io.micronaut.data:micronaut-data-tx")
+testRuntime("io.micronaut.sql:micronaut-jdbc-hikari")
+```
---
+++ micronaut-camunda-bpm-feature/transactional-test/src/test/java/info/novatec/micronaut/camunda/bpm/feature/test/MicronautProcessEngineTransactionalConfigurationTest.java
@@ -0,0 +1,10 @@
+package info.novatec.micronaut.camunda.bpm.feature.test;
+
+import io.micronaut.test.annotation.MicronautTest;
+
+/**
+ * @author Lukasz Frankowski
+ */
+@MicronautTest
+public class MicronautProcessEngineTransactionalConfigurationTest extends MicronautProcessEngineConfigurationTest {
+}
